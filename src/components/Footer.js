474c6771b950aff0e5e71ce94e92efaccfc215a4
Bedrock123 <info@builtbykingwilly.com>
2018-06-24 20:33:12 -0400

    Added in desktop footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,57 @@ class CallOut extends React.Component {
   render() {
     return (
       <div className="footer-wrapper">
-        <p> </p>{" "}
+        <Row style={{ height: "100%" }}>
+          <Col sm={12} md={{ span: 6, offset: 1 }} style={{ height: "100%" }}>
+            <div className="footer-block">
+              <p>Want to join our team!?</p>
+              <br />
+              <a className="button" href="#">
+                join our team
+              </a>
+            </div>
+          </Col>
+          <Col sm={12} md={10} style={{ height: "100%" }}>
+            <div className="footer-block">
+              <img src={logo} className="footer-logo" />
+              <br />
+              <br />
+              <p>nyc | brooklyn | 212.xxx.xxx</p>
+              <p>contact us at catherine@insidecrowd.co</p>
+              <Row>
+                <Col className="social-icons">
+                  <a href="#" target="_blank">
+                    <i class="fab fa-instagram" />
+                  </a>
+                  <a href="#" target="_blank">
+                    <i class="fab fa-facebook-f" />
+                  </a>
+                  <a href="#" target="_blank">
+                    <i class="fab fa-twitter" />
+                  </a>
+                  <a href="#" target="_blank">
+                    <i class="far fa-envelope" />
+                  </a>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+          <Col sm={12} md={6} style={{ height: "100%" }}>
+            <div className="footer-block">
+              <p>Sign up for our newsletter</p>
+              <input
+                label="email"
+                type="email"
+                placeholder="you@amazing.com"
+                className="wf-input"
+              />
+              <br />
+              <a className="button" href="#" style={{ float: "left" }}>
+                submit
+              </a>
+            </div>
+          </Col>
+        </Row>
       </div>
     );
   }
