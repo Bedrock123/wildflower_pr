33203d0e32d7a6ec380c71978fc37b3ff2bd725a
Bedrock123 <info@builtbykingwilly.com>
2019-02-20 13:39:54 -0500

    Added in desktop footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,58 +7,25 @@ class CallOut extends React.Component {
   render() {
     return (
       <div className="footer-wrapper">
-        <Row style={{ height: "100%" }}>
-          <Col
-            md={24}
-            lg={{ span: 6, offset: 1 }}
-            style={{ height: "100%" }}
-            className="mobile-hidden"
-          >
-            <div className="footer-block">
-              <p>Want to join our team!?</p>
-
-              <p>Email jobs@wildflower-pr.com</p>
-            </div>
-          </Col>
-          <Col md={24} lg={10} style={{ height: "100%" }}>
-            <div className="footer-block">
-              <br />
-              <p>nyc | brooklyn | 212.xxx.xxx</p>
-              <p>contact us at catherine@insidecrowd.co</p>
-              <Row>
-                <Col className="social-icons">
-                  <a href="#" target="_blank">
-                    <i class="fab fa-instagram" />
-                  </a>
-
-                  <a href="#" target="_blank">
-                    <i class="far fa-envelope" />
-                  </a>
-                </Col>
-              </Row>
-            </div>
-          </Col>
-          <Col
-            md={24}
-            lg={6}
-            style={{ height: "100%" }}
-            className="mobile-hidden"
-          >
-            <div className="footer-block">
-              <p>Sign up for our newsletter</p>
-              <input
-                label="email"
-                type="email"
-                placeholder="you@amazing.com"
-                className="wf-input"
-              />
-              <br />
-              <a className="button" href="#" style={{ float: "left" }}>
-                submit
-              </a>
-            </div>
-          </Col>
-        </Row>
+        <div className="footer-wrapper-inner">
+          <h3>don't be shy - come bloom among the wild!</h3>
+          <h4>
+            <a href="mailto:hello@wildflower.com">hello@wildflower.com</a>
+          </h4>
+          <div className="sign-up-wrapper">
+            <p>sign up for our newsletter</p>
+            <input
+              label="email"
+              type="email"
+              placeholder=""
+              className="wf-input"
+            />
+          </div>
+          <div className="footer-footer-wrapper">
+            <p>© 2019. All Rights Reserved Wildflower.com</p>
+            <p>Designed by a wholebunchofbricks / Developed Zach Bedrosian</p>
+          </div>
+        </div>
       </div>
     );
   }
